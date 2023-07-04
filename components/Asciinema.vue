83a692181b66ee69afff99675cd6dba319c7dde9
Murilo Cunha <murilo@dataroots.io>
2023-07-04 12:17:25 +0200

    add asciinema component

---
+++ components/Asciinema.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :id="src" />
+</template>
+
+<script>
+import * as AsciinemaPlayer from "asciinema-player";
+
+export default {
+  props: ["src", "playerProps"],
+  mounted() {
+    AsciinemaPlayer.create(this.src, document.getElementById(this.src), this.playerProps);
+  },
+};
+</script>
+
+<style>
+@import "./asciinema-player.css";
+</style>
