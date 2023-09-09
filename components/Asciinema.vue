81ecbfb546d0faa71adb1d2b88ebd3747fc0df6b
Murilo Cunha <murilo@dataroots.io>
2023-09-09 10:46:28 +0100

    format component file

--- components/Asciinema.vue
+++ components/Asciinema.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="src" />
+  <div :id="src" />
 </template>
 
 <script>
@@ -8,7 +8,11 @@ import * as AsciinemaPlayer from "asciinema-player";
 export default {
   props: ["src", "playerProps"],
   mounted() {
-    AsciinemaPlayer.create(import.meta.env.BASE_URL + this.src, document.getElementById(this.src), this.playerProps);
+    AsciinemaPlayer.create(
+      import.meta.env.BASE_URL + this.src,
+      document.getElementById(this.src),
+      this.playerProps
+    );
   },
 };
 </script>
